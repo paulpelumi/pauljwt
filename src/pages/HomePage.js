8fb4dc070c27a3748ba15cbae15ef757b713e645
“Paul <paulpelumi@gmail.com>
2023-03-06 16:05:17 +0100

    initial commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+
+function HomePage() {
+    return (
+      <><div>
+        Home Page
+      </div>
+      <h1>Welcome to Pelumi Paul Homepage</h1>
+      <h2>Here, you'll find all youve been searching for</h2></>
+    );
+  }
+  
+  export default HomePage;
+  
